e8d9d1e33cf792e7ac2e02d75f810d93b20e0014
Regendare <regendare@users.noreply.github.com>
2019-03-08 11:50:05 -0800

    basic input recieveing working, need to figure out how to do movement left / rigth / up / down

--- src/main/main.ino
+++ src/main/main.ino
@@ -30,6 +30,7 @@ void loop()
         movementVertical = Serial.parseInt();
         Serial.print("Vertical: ");
         Serial.println(movementVertical);
+        doVertical(movementVertical);
 
         movementHorizontal = Serial.parseInt();
         Serial.print("Horizontal: ");
@@ -42,9 +43,39 @@ void loop()
         lever = Serial.parseInt();
         Serial.print("Lever: ");
         Serial.println(lever);
+        doLever(lever);
 
         door = Serial.parseInt();
         Serial.print("Door: ");
         Serial.println(door);
+        doDoor(door);
     }
+}
+
+void doDoor(int value) {
+  if (value == 1) {
+    analogWrite(5, 255);
+  } else {
+    analogWrite(5, 0);
+  }
+}
+
+void doLever(int value) {
+  if (value == 1) {
+    analogWrite(6, 255);
+  } else {
+    analogWrite(6, 0);
+  }
+}
+
+void doVertical(int value) {
+  int tempPercentage;
+  int tempRange;
+  if (value == 0) {
+    analogWrite(11, 0);
+  } else if ((value % 2) == 0) { // Even Number
+    tempPercentage = (value / 32720);
+    tempRange = 255 * tempPercentage;
+    analogWrite(11, tempRange);
+  }
 }
