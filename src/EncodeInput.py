094cbe5357c4fb2b46ce0f0fee2005d86e682c54
Regendare <regendare@users.noreply.github.com>
2019-03-08 16:16:08 -0800

    added ded lol zone

--- src/EncodeInput.py
+++ src/EncodeInput.py
@@ -1,9 +1,21 @@
 #!/usr/local/bin/python3
+# Phillip LE bluetooth code (C), 
+# Contributors: Fabio Colacio 
+# ask Phillip Le for permission regarding usage of code.
+# 
 
 import bluetooth
 from sdl2 import *
 
+# -32720 - 32720
+def deadzone(inval, radius):
+    if inval > (0 - radius) and inval < (0 + radius):
+        return 0
+    return inval
+
 if __name__ == '__main__':
+    deadzone_size = 7000
+
     SDL_Init(SDL_INIT_JOYSTICK)
     bd_addr = "98:D3:31:FC:1A:4E"
     bd_port = 1
@@ -34,21 +46,22 @@ if __name__ == '__main__':
         if SDL_QuitRequested():
             quit = True
 
-        data += "{0:06d}".format(SDL_JoystickGetAxis(joy, 0)) # Left Right
+        data += "{0:06d}".format(deadzone(SDL_JoystickGetAxis(joy, 0), deadzone_size)) # Left Right
         data += " "
-        data += "{0:06d}".format(SDL_JoystickGetAxis(joy, 1)) # Up Down
+        data += "{0:06d}".format(deadzone(SDL_JoystickGetAxis(joy, 1), deadzone_size)) # Up Down
         data += " "
-        data += "{0:06d}".format(SDL_JoystickGetAxis(joy, 2)) # Rotation Left Right
+        data += "{0:06d}".format(deadzone(SDL_JoystickGetAxis(joy, 2), deadzone_size)) # Rotation Left Right
         data += " "
         data += str(SDL_JoystickGetButton(joy, 0)) # B Button
         data += " "
         data += str(SDL_JoystickGetButton(joy, 1)) # A Button
+        data += "\n"
 
-        print(data)
+        print(data, end="")
         bd_sock.send(data)
 
         data = ""
-        SDL_Delay(1500)
+        SDL_Delay(150)
     
     SDL_Quit()
     bd_sock.close()
--- src/main/main.ino
+++ src/main/main.ino
@@ -10,61 +10,39 @@ int door;
 char c;
 SoftwareSerial BTSerial(RxD, TxD);
 
+#define LED_GREEN 5
+#define LED_WHITE 6
+#define LED_BLUE 10
+#define LED_PINK 11
+#define LED_IDK 12
+
 void setup()
 {
     BTSerial.flush();
     delay(25);
     BTSerial.begin(9600);
     Serial.begin(9600);  //Sets the baud for serial data transmission
-    pinMode(12, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(5, OUTPUT);
-}
-
-void loop()
-{
-    if (Serial.available() >= 24) // Send data only when you receive data:
-    {
-        movementVertical = Serial.parseInt();
-        Serial.print("Vertical: ");
-        Serial.println(movementVertical);
-        doVertical(movementVertical);
-
-        movementHorizontal = Serial.parseInt();
-        Serial.print("Horizontal: ");
-        Serial.println(movementHorizontal);
-
-        movementRotation = Serial.parseInt();
-        Serial.print("Rotation: ");
-        Serial.println(movementRotation);
-
-        lever = Serial.parseInt();
-        Serial.print("Lever: ");
-        Serial.println(lever);
-        doLever(lever);
-
-        door = Serial.parseInt();
-        Serial.print("Door: ");
-        Serial.println(door);
-        doDoor(door);
-    }
+    
+    pinMode(LED_IDK, OUTPUT);
+    pinMode(LED_PINK, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_WHITE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
 }
 
 void doDoor(int value) {
   if (value == 1) {
-    analogWrite(5, 255);
-  } else {
-    analogWrite(5, 0);
+    analogWrite(LED_WHITE, 255);
+  } else if (value == 0) {
+    analogWrite(LED_WHITE, 0);
   }
 }
 
 void doLever(int value) {
   if (value == 1) {
-    analogWrite(6, 255);
-  } else {
-    analogWrite(6, 0);
+    analogWrite(LED_GREEN, 255);
+  } else if (value == 0) {
+    analogWrite(LED_GREEN, 0);
   }
 }
 
@@ -78,4 +56,33 @@ void doVertical(int value) {
     tempRange = 255 * tempPercentage;
     analogWrite(11, tempRange);
   }
+} 
+
+void loop()
+{
+    if (Serial.available() >= 25) // Send data only when you receive data:
+    {
+        movementVertical = Serial.parseInt();
+        Serial.print("Vertical: ");
+        Serial.println(movementVertical);
+        doVertical(movementVertical);
+
+        movementHorizontal = Serial.parseInt();
+        Serial.print("Horizontal: ");
+        Serial.println(movementHorizontal);
+
+        movementRotation = Serial.parseInt();
+        Serial.print("Rotation: ");
+        Serial.println(movementRotation);
+  
+        lever = Serial.parseInt();
+        Serial.print("Lever: ");
+        Serial.println(lever);
+        doLever(lever);
+
+        door = Serial.parseInt();
+        Serial.print("Door: ");
+        Serial.println(door);
+        doDoor(door);
+    }
 }
