74980fb7b47716492f9fa037f7b624d0963716e8
Regendare <regendare@users.noreply.github.com>
2019-03-15 01:24:11 -0700

    idk what the f is wrong lmao. tomorrow is compeition!

--- src/Controller_Events.py
+++ src/Controller_Events.py
@@ -2,11 +2,34 @@ import ctypes
 import time
 from sdl2 import *
 import bluetooth
+import math
 
-def deadzone(inval, radius):
+def calcDeadZone(inval, radius):
     if inval > (0 - radius) and inval < (0 + radius):
-        return False
-    return True
+        return 0
+    return inval
+
+def idlePrevent(value):
+    if value == 168.5:
+        return 255
+    return value
+
+# map_val maps a value val from the range [inmin, inmax] to the range [outmin, outmax]
+def map_val(val, inmin, inmax, outmin, outmax):
+    return (val - inmin) * (outmax - outmin)  / (inmax - inmin) + outmin
+
+# mag_dir returns a tuple with the magnitude and direction of the joystick (in radians),
+# and the magnitude [0, 1]. The inputs should be in the range [-1.0, 1.0]
+def mag_dir(xval, yval):
+    m = math.hypot(xval, yval)
+    d = math.atan2(yval, xval)
+    return (m, d)
+
+xraw = 0
+yraw = 0
+speedMin = 83.0
+speedMax = 254.0
+
 
 if __name__ == "__main__":
     SDL_Init(SDL_INIT_JOYSTICK)
@@ -22,51 +45,138 @@ if __name__ == "__main__":
     while True:
         event = SDL_Event()
         while SDL_PollEvent(ctypes.byref(event)) != 0:
-
             '''
             BUTTON EVENTS, listens for up and down events
 
             Button A - 0
             Button B - 1
+            BUTTON Y - 2
+            BUTTON X - 3
+
+            BUTTON DPAD UP    - 11
+            BUTTON DPAD DOWN  - 12
+            BUTTON DPAD LEFT  - 13 
+            BUTTON DPAD RIGHT - 14
             
+            BUTTON 4 - MINUS
+            BUTTON 5 - PLUS
+
+            RIGHT BUMPER - 10
+            LEFT BUMPER  - 9
             '''
             if event.type == SDL_JOYBUTTONDOWN:
                 if event.jbutton.button == 0:
                     print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
-                    BD_SOCK.send("3 {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
                 elif event.jbutton.button == 1:
                     print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
-                    BD_SOCK.send("3 {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 2:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 3:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 9:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 10:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 11:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 12:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 13:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 14:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 8:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 7:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
             elif event.type == SDL_JOYBUTTONUP:
                 if event.jbutton.button == 0:
                     print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
-                    BD_SOCK.send("4 {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
                 elif event.jbutton.button == 1:
                     print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
-                    BD_SOCK.send("4 {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 2:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 3:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 9:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 10:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 11:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 12:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 13:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 8:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+                elif event.jbutton.button == 7:
+                    print("Button: {} - State: {}".format(event.jbutton.button, SDL_JoystickGetButton(joy, event.jbutton.button)))
+                    BD_SOCK.send(str(event.jbutton.button) + " {0:06d}".format(SDL_JoystickGetButton(joy, event.jbutton.button)))
+
 
             '''
-            AXIS EVENTS, listens for axis value changes
-
-            Axis 0 - Movement Up and Down
-            Axis 1 - Movement Left and Right
-            Axis 2 - Rotation Left and Right
+            DUMMY VALUE FRONTLEFT  - 15
+            DUMMY VALUE FRONTRIGHT - 16
+            DUMMY VALUE BACKLEFT   - 17
+            DUMMY VALUE BACKRIGHT  - 18
             '''
+            # if event.type == SDL_JOYAXISMOTION:
+            #     if event.jaxis.axis == 0 or event.jaxis.axis == 1:
 
-            if event.type == SDL_JOYAXISMOTION:
-                if event.jaxis.axis == 0:
-                    value = event.jaxis.value
-                    if deadzone(value, DEADZONE):
-                        print("Axis: {} - Value: {}".format(event.jaxis.axis, event.jaxis.value))
-                        BD_SOCK.send(str(event.jaxis.axis) + " {0:06d}".format(event.jaxis.value))
-                if event.jaxis.axis == 1:
-                    value = event.jaxis.value
-                    if deadzone(value, DEADZONE):
-                        print("Axis: {} - Value: {}".format(event.jaxis.axis, event.jaxis.value))
-                        BD_SOCK.send(str(event.jaxis.axis) + " {0:06d}".format(event.jaxis.value))
-                if event.jaxis.axis == 2:
-                    value = event.jaxis.value
-                    if deadzone(value, DEADZONE):
-                        print("Axis: {} - Value: {}".format(event.jaxis.axis, event.jaxis.value))
-                        BD_SOCK.send(str(event.jaxis.axis) + " {0:06d}".format(event.jaxis.value))
                 
+            #         value = event.jaxis.value
+            #         tempx = SDL_JoystickGetAxis(joy, 1)
+            #         tempy = SDL_JoystickGetAxis(joy, 0)
+            #         xraw = calcDeadZone(tempx, DEADZONE)
+            #         yraw = calcDeadZone(tempy, DEADZONE)
+            #         x = map_val(xraw, -33000, 33000, -1.0, 1.0)
+            #         y = map_val(yraw, -33000, 33000, -1.0, 1.0)
+
+            #         m, d = mag_dir(x, y)
+
+            #         frontleft = m * math.sin((math.pi / 4) - d) - m
+            #         frontright = m * math.cos((math.pi / 4) - d) + m
+            #         backleft = m * math.cos((math.pi / 4) - d) - m
+            #         backright = m * math.sin((math.pi / 4) - d) + m
+
+            #         frontleft = map_val(frontleft, -1.0, 1.0, speedMin, speedMax)
+            #         frontright = map_val(frontright, -1.0, 1.0, speedMin, speedMax)
+            #         backleft = map_val(backleft, -1.0, 1.0, speedMin, speedMax)
+            #         backright = map_val(backright, -1.0, 1.0, speedMin, speedMax)
+
+            #         print("Front Left: ", idlePrevent(frontleft))
+            #         print("Front Right: ", idlePrevent(frontright))
+            #         print("Back Left: ", idlePrevent(backleft))
+            #         print("Back Right: ", idlePrevent(backright))
+
+            #         BD_SOCK.send("15 {0:06f}".format(frontleft))
+            #         BD_SOCK.send("16 {0:06f}".format(frontright))
+            #         BD_SOCK.send("17 {0:06f}".format(backleft))
+            #         BD_SOCK.send("18 {0:06f}".format(backright))
+            #     elif event.jaxis.axis == 2:
+            #         tempr = SDL_JoystickGetAxis(joy, 2)
+            #         tempr = map_val(tempr, -33000.0, 33000.0, speedMin, speedMax)
+            #         print("Rotation Pos: ", idlePrevent(tempr))
--- src/main/main.ino
+++ src/main/main.ino
@@ -1,12 +1,31 @@
 #include <SoftwareSerial.h>
 #define RxD 10
 #define TxD 11
+
+SoftwareSerial BTSerial(RxD, TxD);
+
 String data; //Variable for storing received data
 int firstSet;
 int secondSet;
-SoftwareSerial BTSerial(RxD, TxD);
 
-#define PIN_LEVER 11
+
+// SPEED CONSTANTS
+#define FORWARD 83
+#define BACKWARD 220
+#define STOP 255
+
+
+// MOTOR PINS
+#define MOTOR_FL 12
+#define MOTOR_FR 11
+#define MOTOR_BL 10
+#define MOTOR_BR 13
+#define MOTOR_LL 8
+#define MOTOR_LR 7
+#define MOTOR_TL 6
+#define MOTOR_TR 5
+#define MOTOR_DL 4
+#define MOTOR_DR 3
 
 void setup()
 {
@@ -14,8 +33,17 @@ void setup()
     delay(25);
     BTSerial.begin(9600);
     Serial.begin(9600);  //Sets the baud for serial data transmission
-    
-    pinMode(PIN_LEVER, OUTPUT);
+
+    pinMode(MOTOR_FL, OUTPUT);
+    pinMode(MOTOR_FR, OUTPUT);
+    pinMode(MOTOR_DL, OUTPUT);
+    pinMode(MOTOR_DR, OUTPUT);
+    pinMode(MOTOR_LR, OUTPUT);
+    pinMode(MOTOR_LL, OUTPUT);
+    pinMode(MOTOR_TL, OUTPUT);
+    pinMode(MOTOR_TR, OUTPUT);
+    pinMode(MOTOR_BL, OUTPUT);
+    pinMode(MOTOR_BR, OUTPUT);
 }
 
 void loop()
@@ -24,37 +52,205 @@ void loop()
     {
         firstSet = Serial.parseInt();
         Serial.print(firstSet);
+        secondSet = Serial.parseInt();
+        Serial.print(secondSet);
+        // FORWARD FUNCTION
 
-        // Axis Movement - Up Down
-        if (firstSet == 0) {
-          secondSet = Serial.parseInt();
+        // UP
+        if (firstSet == 11) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, BACKWARD);
+                analogWrite(MOTOR_BL, BACKWARD);
+                analogWrite(MOTOR_FR, FORWARD);
+                analogWrite(13, FORWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
 
-        // Axis Movement - Left Right
+            // DOWN
+        } else if (firstSet == 12) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, FORWARD);
+                analogWrite(MOTOR_BL, FORWARD);
+                analogWrite(MOTOR_FR, BACKWARD);
+                analogWrite(MOTOR_BR, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
+
+            // LEFT
+        } else if (firstSet == 13) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, BACKWARD);
+                analogWrite(MOTOR_BL, FORWARD);
+                analogWrite(MOTOR_FR, BACKWARD);
+                analogWrite(MOTOR_BR, FORWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
+
+            // RIGHT
+        } else if (firstSet == 14) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, FORWARD);
+                analogWrite(MOTOR_BL, BACKWARD);
+                analogWrite(MOTOR_FR, FORWARD);
+                analogWrite(MOTOR_BR, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
+
+            // A BUTTON -- LEFT LEVER UP
+        } else if (firstSet == 0) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_LL, FORWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_LL, STOP);
+            } else {
+                analogWrite(MOTOR_LL, STOP);
+            }
+
+            // B BUTTON -- LEFT LEVER DOWN
         } else if (firstSet == 1) {
-          secondSet = Serial.parseInt();
+            if (secondSet == 1) {
+                analogWrite(MOTOR_LL, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_LL, STOP);
+            } else {
+                analogWrite(MOTOR_LL, STOP);
+            }
 
-        // Axis Rotation - Left Right
+            // Y BUTTON -- RIGHT LEVER UP
         } else if (firstSet == 2) {
-          secondSet = Serial.parseInt();
+            if (secondSet == 1) {
+                analogWrite(MOTOR_LR, 120);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_LR, STOP);
+            } else {
+                analogWrite(MOTOR_LR, STOP);
+            }
 
-        // Button A - Up
+            // Y BUTTON -- RIGHT LEVER DOWN
         } else if (firstSet == 3) {
-          secondSet = Serial.parseInt();
             if (secondSet == 1) {
-              analogWrite(PIN_LEVER, 255);
+                analogWrite(MOTOR_LR, 190);
             } else if (secondSet == 0) {
-              analogWrite(PIN_LEVER, 0);
+                analogWrite(MOTOR_LR, STOP);
+            } else {
+                analogWrite(MOTOR_LR, STOP);
             }
-        // Button B - Down
-        } else if (firstSet == 4) {
-          secondSet = Serial.parseInt();
 
+            // LEFT BUMPER -- 
+        } else if (firstSet == 9) {
             if (secondSet == 1) {
-              analogWrite(PIN_LEVER, 255);
+                analogWrite(MOTOR_DR, FORWARD);
+                analogWrite(MOTOR_DL, BACKWARD);
             } else if (secondSet == 0) {
-              analogWrite(PIN_LEVER, 0);
+                analogWrite(MOTOR_DR, STOP);
+                analogWrite(MOTOR_DL, STOP);
+            } else {
+                analogWrite(MOTOR_DR, STOP);
+                analogWrite(MOTOR_DL, STOP);
             }
-          
+        } else if (firstSet == 10) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_DR, BACKWARD);
+                analogWrite(MOTOR_DL, FORWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_DR, STOP);
+                analogWrite(MOTOR_DL, STOP);
+            } else {
+                analogWrite(MOTOR_DR, STOP);
+                analogWrite(MOTOR_DL, STOP);
+            }
+        } else if (firstSet == 4) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, FORWARD);
+                analogWrite(MOTOR_FR, BACKWARD);
+                analogWrite(MOTOR_BL, FORWARD);
+                analogWrite(MOTOR_BR, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
+        } else if (firstSet == 6) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_FL, FORWARD);
+                analogWrite(MOTOR_FR, BACKWARD);
+                analogWrite(MOTOR_BL, FORWARD);
+                analogWrite(MOTOR_BR, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            } else {
+                analogWrite(MOTOR_FL, STOP);
+                analogWrite(MOTOR_BL, STOP);
+                analogWrite(MOTOR_FR, STOP);
+                analogWrite(MOTOR_BR, STOP);
+            }
+        } else if (firstSet == 7) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_DL, FORWARD);
+                analogWrite(MOTOR_DR, FORWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_DL, STOP);
+                analogWrite(MOTOR_DR, STOP);
+            } else {
+                analogWrite(MOTOR_DL, STOP);
+                analogWrite(MOTOR_DR, STOP);
+            }
+        } else if (firstSet == 8) {
+            if (secondSet == 1) {
+                analogWrite(MOTOR_DL, BACKWARD);
+                analogWrite(MOTOR_DR, BACKWARD);
+            } else if (secondSet == 0) {
+                analogWrite(MOTOR_DL, STOP);
+                analogWrite(MOTOR_DR, STOP);
+            } else {
+                analogWrite(MOTOR_DL, STOP);
+                analogWrite(MOTOR_DR, STOP);
+            }
         }
     }
 }
